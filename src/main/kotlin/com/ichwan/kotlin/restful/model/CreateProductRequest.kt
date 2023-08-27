89fd2de93380d7407c3ec90ca43f26a3fa1169fe
ichwansh03 <ichwansholihin03@gmail.com>
2023-08-28 06:21:43 +0700

    chore (model): add annotation field in CreateProductRequest

--- src/main/kotlin/com/ichwan/kotlin/restful/model/CreateProductRequest.kt
+++ src/main/kotlin/com/ichwan/kotlin/restful/model/CreateProductRequest.kt
@@ -1,8 +1,22 @@
 package com.ichwan.kotlin.restful.model
 
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
 data class CreateProductRequest(
+
+    @field:NotBlank
     val id: String,
+
+    @field:NotBlank
     val name: String,
+
+    @field:NotNull
+    @field:Min(value = 1)
     val price: Long,
+
+    @field:NotNull
+    @field:Min(value = 1)
     val quantity: Int
 )
